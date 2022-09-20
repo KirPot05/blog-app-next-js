34a7393aa9316e2d71fd4b58f084a320499e770a
Kiran <kiran@potdar.com>
2022-09-20 16:15:07 +0530

    added comments section in post-page, refactored header for mobile screens

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -4,12 +4,33 @@ import Header from '../../components/Header';
 import {sanityClient, urlFor} from '../../sanity';
 import { Post } from '../../types';
 import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form';
 
 type Props = {
     post: Post;
 }
 
+type InputFormTypes = {
+    _id: string;
+    name: string;
+    email: string;
+    comment: string;
+}
+
 function Post({post}: Props) {
+
+    const { register, handleSubmit, formState: {errors} } = useForm<InputFormTypes>();
+
+    const postComment: SubmitHandler<InputFormTypes> = async (data) => {
+        try{
+            const res = await fetch('/api/comments/create', {
+                method: 'POST',
+                body: JSON.stringify(data)
+            })
+        } catch(err){
+            console.log(err);
+        }
+    }
 
     return (
         <main className='max-w-7xl mx-auto'>
@@ -59,6 +80,40 @@ function Post({post}: Props) {
                     />
                 </div>
             </article>
+
+            <hr className='max-w-lg my-5 mx-auto border border-yellow-500' />
+
+            <form onSubmit={handleSubmit(postComment)} className='flex flex-col p-5 max-w-2xl mx-auto mb-10'>
+                <h3 className='text-sm text-yellow-500'> Enjoyed this article? </h3>
+                <h2 className='text-3xl font-bold'> Leave a comment below! </h2>
+                <hr className='py-3 mt-2' />
+                
+                <input {...register("_id")} type="hidden" name='_id' value={post._id} />
+
+                <label className='flex flex-col mb-5 space-y-2'>
+                    <span className='text-gray-700'>Name</span>
+                    <input {...register("name", {required: true})} name="name" placeholder='Enter your name' type="text" className='p-2 shadow border rounded outline-none focus:ring-2 ring-yellow-500' />
+                </label>
+
+                <label className='flex flex-col mb-5 space-y-2'>
+                    <span className='text-gray-700'>Email</span>
+                    <input {...register("email", {required: true})} name="email" placeholder='example@sample.com' type='email' className='p-2 shadow border rounded outline-none focus:ring-2 ring-yellow-500' />
+                </label>
+                
+                <label className='flex flex-col mb-5 space-y-2'>
+                    <span className='text-gray-700'>Comment</span>
+                    <textarea {...register("comment", {required: true})} name="comment" placeholder='Enter your views...' className='p-2 shadow border rounded outline-none  focus:ring-2 ring-yellow-500' cols={50} rows={5}></textarea>
+                </label>
+
+                {/* Errors after validation of input data */}
+                <div className='p-2 space-y-3 mb-3'>
+                    {errors.name && <p className='text-red-500 text-sm'> Name field is required </p>}
+                    {errors.email && <p className='text-red-500 text-sm'> Email is required </p>}
+                    {errors.comment && <p className='text-red-500 text-sm'> Comment is required </p>}
+                </div>
+
+                <input type="submit" className='shadow text-lg bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-semibold py-2 px-4 rounded cursor-pointer' value="Submit" />
+            </form>
 
       </main>
     )
